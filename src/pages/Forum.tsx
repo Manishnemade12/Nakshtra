c9b759f8bcec053d55813fa2db682616268e0a29
Manish Nemade <166635137+Manishnemade12@users.noreply.github.com>
2025-08-23 01:49:06 +0530

    Update Forum.tsx

--- src/pages/Forum.tsx
+++ src/pages/Forum.tsx
@@ -4,7 +4,7 @@ const Forum = () => {
   return (
     <div style={{ height: "100vh", width: "100vw", margin: 0, padding: 0, overflow: "hidden" }}>
       <iframe
-        src="http://localhost:8081/"
+        src="https://uni-chatter-wall.vercel.app/"
         title="Forum"
         style={{
           border: "none",
